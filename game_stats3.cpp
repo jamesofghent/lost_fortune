f5da3b70e2a90eaa7e46baab333f558b5445effc
james wilson <jamesofghent@gmail.com>
2015-09-17 07:39:18 -0400

    game_stats3

---
+++ game_stats3.cpp
@@ -0,0 +1,25 @@
+//Game Stats 3.0
+//Demonstrating constants
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  
+  const int ALIEN_POINTS=150;
+  int aliensKilled=10;
+  int score=aliensKilled * ALIEN_POINTS;
+  cout <<"score: "<<score<<endl;
+  
+  enum difficulty {NOVICE, EASY, NORMAL, HARD, UNBEATABLE};
+  difficulty myDifficulty=EASY;
+
+  enum shipCost {FIGHTER_COST=25, BOMBER_COST, CRUISER_COST=50};
+  shipCost myShipCost=BOMBER_COST;
+  cout <<"\nTo upgade my ship to a Cruiser will cost "
+       << (CRUISER_COST-myShipCost)<< " Reasource Points.\n";
+
+  return 0;
+
+}   
